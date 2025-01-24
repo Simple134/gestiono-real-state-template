7fdfcdb48784208ba5ba7e462ac894f82a823c6c
Simple134 <sanchezjosue2001@gmail.com>
2025-01-23 21:19:11 -0400

    Update cityCard.tsx

--- src/components/cityCard.tsx
+++ src/components/cityCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const cityBackgrounds = [
     "/lavegaimage.png",
-    "/mocaImage.png",
+    "/mocaimage.png",
     "/santiagoimage.png",
     "/puntaCanaimage.png",
 ]
