d10c8495659a066d6bc1d7b1a0b9fd7961bbd034
Simple134 <sanchezjosue2001@gmail.com>
2025-01-27 22:29:28 -0400

    .

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,4 +1,4 @@
-import { gestiono } from '@/gestiono';
+import { Gestiono } from "@bitnation-dev/management/dist/package/src/server";
 import { NextRequest, NextResponse } from 'next/server';
 
 
@@ -8,16 +8,18 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
     if (!process.env.GESTIONO_API_URL) {
       throw new Error('GESTIONO_API_URL is not defined');
     }
-    const properties = await gestiono.getResources() 
+    const properties = await Gestiono.getResources() 
+    console.log(properties, "properties");
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const data = properties.map((property: any) => ({
       id: property?.id,
       title: property?.name,
-      price: property?.sellPrice,
-      location: property?.location,
       description: property?.description,
+      price: property?.sellPrice,
+      location: null,
       image: property?.multimedia.map((media: {url: string}) => media.url),
     }));
+    console.log(data, "data");
     return NextResponse.json(data); 
   } catch (error) {
     console.error('Error al obtener las casas:', error);
--- src/gestiono.ts
+++
@@ -1,2 +0,0 @@
-import { Gestiono } from "@bitnation-dev/management/dist/package/src/server";
-export const gestiono = Gestiono;
